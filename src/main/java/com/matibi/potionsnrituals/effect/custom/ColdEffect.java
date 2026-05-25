aa68d7bbe700acbb805ed68e4ec83440ee9873af
Matibi <mathias.bagal@gmail.com>
2026-05-25 13:54:27 +0200

    ajout de plusieurs effets et icones

--- src/main/java/com/matibi/potionsnrituals/effect/custom/ColdEffect.java
+++ src/main/java/com/matibi/potionsnrituals/effect/custom/ColdEffect.java
@@ -7,7 +7,6 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 
 public class ColdEffect extends MobEffect {
--- src/main/java/com/matibi/potionsnrituals/mixin/effect/AsthmaMixin.java
+++ src/main/java/com/matibi/potionsnrituals/mixin/effect/AsthmaMixin.java
@@ -1,7 +1,6 @@
 package com.matibi.potionsnrituals.mixin.effect;
 
 import com.matibi.potionsnrituals.effect.ModEffects;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
